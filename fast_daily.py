3c84bcfba56db92f6695b1ea9f5e2c7521499f69
n1k <nik726@gmail.com>
2020-05-19 22:19:36 +0700

    Разбивает на функции

--- fast_daily.py
+++ fast_daily.py
@@ -1,6 +1,39 @@
 import csv
 from collections import defaultdict
 
+FIRST_HOUR = '00'
+LAST_HOUR = '23'
+
+
+def sum_lifts_wo_moving(d: dict):
+    daily_sum = defaultdict(int)
+
+    for k in d:
+        try:
+            if d[k][FIRST_HOUR] == d[k][LAST_HOUR]:
+                daily_sum[k[0]] += 1
+        except KeyError:
+            pass
+
+    return daily_sum
+
+
+def write_dict(d: dict, filename='daily_sum.csv'):
+    with open(filename, 'w') as f:
+        for k in d:
+            f.write("{};{}\n".format(k, d[k]))
+
+
+# на случай если мы захотим увидеть не сумму, а список дат, когда не ездил лифт
+def write_date_lift_wo_moving(d: dict, filename='daily.csv'):
+    with open(filename, 'w') as f:
+        for k in d:
+            try:
+                if d[k][FIRST_HOUR] == d[k][LAST_HOUR]:
+                    f.write("{};{}\n".format(k[0], k[1]))
+            except KeyError:
+                pass
+
 
 def main():
     filename = 'statdriv.csv'
@@ -17,39 +50,11 @@ def main():
             hour = dt[11:13]
 
             key = day, lift
-
-            if hour == '00' and key not in d or hour == '23':
+            if (hour == FIRST_HOUR and key not in d) or hour == LAST_HOUR:
                 d[key][hour] = num
-            # if hour == '00':
-            #     if key not in d:
-            #         d[key]['start'] = num
-            # elif hour == '23':
-            #     d[key]['stop'] = num
 
-
-    daily = defaultdict(int)
-    for x in d:
-        try:
-            if d[x]['00'] == d[x]['23']:
-                daily[x[0]] += 1
-        except:
-            pass
-
-    with open('new_daily_sum.csv', 'w') as f:
-
-        for x in daily:
-            f.write("{};{}\n".format(x, daily[x]))
-
-
-
-
-    with open('new_daily.csv', 'w') as f:
-        for x in d:
-            try:
-                if d[x]['00'] == d[x]['23']:
-                    f.write("{};{}\n".format(x[0], x[1]))
-            except:
-                pass
+    daily = sum_lifts_wo_moving(d)
+    write_dict(daily)
 
 
 if __name__ == '__main__':
