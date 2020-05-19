bc9d46474b3787001d0ef2bf925086d1132ca4d7
n1k <nik726@gmail.com>
2020-05-19 20:17:42 +0700

    Заменяет сплиты на слайсы и ещё ускорят обработку

--- fast_daily.py
+++ fast_daily.py
@@ -1,21 +1,20 @@
 import csv
-from datetime import datetime, timedelta
 from collections import defaultdict
 
 
 def main():
     filename = 'statdriv.csv'
     csv.register_dialect('win', delimiter=';')
-    date_format = "%Y-%m-%d %H:%M:%S.%f"
+
     d = defaultdict(dict)
     with open(filename, 'r') as csvfile:
         reader = csv.reader(csvfile, dialect='win')
         _ = reader.__next__()
 
         for lift, dt, num in reader:
-
-            day, time = dt[:len(dt) - 4].split()
-            hour = time.split(':')[0]
+            # 19.11.2019 00:00:00.000
+            day = dt[:10]
+            hour = dt[11:13]
 
             key = day, lift
 
@@ -25,19 +24,30 @@ def main():
             elif hour == '23':
                 d[key]['stop'] = num
 
-    with open('new_daily.csv', 'w') as f:
-        for x in d:
-            try:
-                if d[x]['stop'] == d[x]['start']:
-                    f.write("{};{}\n".format(x[0], x[1]))
-            except:
-                pass
+
+    daily = defaultdict(int)
+    for x in d:
+        try:
+            if d[x]['stop'] == d[x]['start']:
+                daily[x[0]] += 1
+        except:
+            pass
+
+    with open('new_daily_sum.csv', 'w') as f:
+
+        for x in daily:
+            f.write("{};{}\n".format(x, daily[x]))
 
 
 
 
-
-
+    # with open('new_daily.csv', 'w') as f:
+    #     for x in d:
+    #         try:
+    #             if d[x]['stop'] == d[x]['start']:
+    #                 f.write("{};{}\n".format(x[0], x[1]))
+    #         except:
+    #             pass
 
 
 if __name__ == '__main__':
